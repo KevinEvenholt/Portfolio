08138b0b26fc35d298f6f1a37ab5f557372f76cf
Kevin Evenholt <kevin@momang.io>
2024-09-06 12:46:46 +0200

    new favicon light for lightmode and dark for darkmode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,20 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Kevin Evenholt",
   description: "Portfolio website for Kevin Evenholt, a frontend developer.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/icon-dark.svg",
+        href: "/icon-dark.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/icon-light.svg",
+        href: "/icon-light.svg",
+      },
+    ],
+  },
 };
 
 const playfair = Playfair_Display({
